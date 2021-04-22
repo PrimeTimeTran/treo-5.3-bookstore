afac8692df207e11d5b23e7c04ea3c180fbc5fb9
Loi Van Tran <datloiboi@gmail.com>
2021-04-22 16:16:47 +0700

    Got it working in production

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Nav, Card, Container, Row } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+
+
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+const bookData = require('./utils/db.json')
+
+// 1. Fetch Book Data
+// 2. Render cards to the screen which have the books data
+// 3. Define the behavior of viewing one book.
+// 4. Define a page which displays the details of an individual book.
+// 5. Search for books on search term.
+// 6. Limit the number of results.
+// 7. 
+
+function BookDetailPage() {
+    const { id } = useParams()
+    const [book, setBook] = useState({});
+
+    useEffect(() => {
+      async function fetchBook() {
+        const resp = await fetch("http://localhost:5000/books/" + id);
+        const json = await resp.json();
+        console.log({ json });
+        setBook(json);
+      }
+      fetchBook();
+    }, []);
+  return (
+    <div>
+      <h1>Book details!</h1>
+    </div>
+  )
+}
+
+function HomePage() {
+  const [pageNum, setPageNum] = useState(1)
+  const [query, setQuery] = useState('')
+  const [limit, setLimit] = useState(3)
+  const [books, setBooks] = useState([]);
+
+  async function fetchBooks(newQuery) {
+    let urlParams = `?_page=${pageNum}&_limit=${limit}`;
+    if (query !== '') {
+      urlParams = urlParams + `&q=${query}`
+    }
+
+    let json
+    
+    if (process.env.NODE_ENV === "Production") {
+    // if (true) {
+      console.log('Production')
+      json = bookData.books;
+      if (query !== '') {
+        json = json.filter((b) => {
+          console.log({
+            newQuery,
+            bookTitle: b.title,
+            formattedTitle: b.title.toLowerCase(), 
+            matchesQuery: !b.title.toLowerCase().search(newQuery),
+          });
+          return !b.title.toLowerCase().search(newQuery)
+        })
+      }
+    } else {
+      console.log('Development')
+      const resp = await fetch("http://localhost:5000/books" + urlParams);
+      json = await resp.json();
+    }
+    
+    setBooks(json);
+  }
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const onSearch = (e) => {
+    setQuery(e.target.value)
+    fetchBooks(e.target.value);
+  }
+
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <input onChange={onSearch} />
+      <Row>
+        {books.map((b) => {
+          return (
+            <Card key={b.id} className="m-3" style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={"http://localhost:5000/" + b.imageLink}
+              />
+              <Card.Body>
+                <Card.Title>{b.title}</Card.Title>
+                <Card.Text>{b.author}</Card.Text>
+                <Card.Text>{b.language}</Card.Text>
+                <Nav.Link as={Link} to={"/books/" + b.id}>
+                  View
+                </Nav.Link>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </Row>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/books/:id" exact component={BookDetailPage} />
+        </Switch>
+      </Container>
     </div>
   );
 }
